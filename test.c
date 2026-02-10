066f2e1a7fe12454ac4f667bc18f12b1ecddadce
Anjana-NK <anjana83nk@gmail.com>
2026-02-10 17:07:28 +0530

    motham error aan

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char age;
+    printf("enter age")
+    scanf("%d",age);
+    if (age<18){
+        printf("eligible to vote");
+    else{
+        printf("not eligible");
+    }
+    return 0;
+    }
+}
